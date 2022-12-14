883ed2be4a3129dff5c85652e5db7bb7eade74a5
Aramson <82621068+aramflh@users.noreply.github.com>
2022-12-14 02:39:26 +0100

    Ok

--- keycloak/deployKeacloak.sh
+++ keycloak/deployKeacloak.sh
@@ -3,8 +3,32 @@
 # Run as sudo
 
 # Install docker
-apt update -y
-install docker.io -y
+apt-get remove docker -y
+apt-get remove docker-engine d -y
+apt-get remove docker.io -y
+apt-get remove containerd -y
+apt-get remove runc -y
+
+apt-get update -y
+
+apt-get install ca-certificates -y
+apt-get install curl -y
+apt-get install gnupg -y
+apt-get install lsb-release -y
+
+mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update -y
+
+apt-get install docker-ce -y
+apt-get install docker-ce-cli -y
+apt-get install containerd.io -y
+apt-get install docker-compose-plugin -y
+
 
 # Deploy Postgres DB
 
