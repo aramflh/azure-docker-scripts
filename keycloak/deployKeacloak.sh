cdcd606e9983a8a6f5fc89be13a5ff4575096a48
Aramson <82621068+aramflh@users.noreply.github.com>
2022-12-14 02:09:30 +0100

    OK

--- keycloak/deployKeacloak.sh
+++ keycloak/deployKeacloak.sh
@@ -3,18 +3,8 @@
 # Run as sudo
 
 # Install docker
-
-sudo apt-get remove docker docker-engine docker.io
-sudo apt-get remove docker -y
-sudo apt-get remove docker-engine -y
-sudo apt-get remove docker.io -y
-
-apt-get update -y
-apt-get install docker-ce -y
-apt-get install docker-ce-cli -y
-apt-get install containerd.io -y
-apt-get install docker-compose-plugin -y
-apt-get install docker-compose -y
+apt update -y
+install docker.io -y
 
 # Deploy Postgres DB
 
@@ -22,4 +12,4 @@ docker run --name postgres-db -e POSTGRES_USER=$postgres_user -e POSTGRES_PASSWO
 
 # Deploy keycloak
 
-docker run -p $exposed_port:8080 -e KEYCLOAK_ADMIN=$keycloak_user -e KEYCLOAK_ADMIN_PASSWORD=$keycloak_pwd quay.io/keycloak/keycloak:20.0.1 start
+docker run -p $exposed_port:8080 -e KEYCLOAK_ADMIN=$keycloak_user -e KEYCLOAK_ADMIN_PASSWORD=$keycloak_pwd -e KC_DB=$kc_db -e KC_HOSTNAME=$kc_hostname -e KC_DB_PASSWORD=$postgres_pwd -e KC_DB_USERNAME=$postgres_user quay.io/keycloak/keycloak:20.0.1 start
